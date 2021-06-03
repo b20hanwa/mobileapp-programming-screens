0b495a0a59c2abe5db67fa44237a24dc058b5fc4
b20hanwa <b20hanwa@student.his.se>
2021-06-03 18:06:48 +0200

    Added OnClick to LoginActivity

--- app/src/main/java/com/example/screens/LoginActivity.java
+++ app/src/main/java/com/example/screens/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.screens;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -7,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -21,6 +23,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.open);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
