adcc79f52d24c3a7d920fc5c943dea1e36f5ecf5
b20hanwa <b20hanwa@student.his.se>
2021-06-03 17:32:37 +0200

    Added LoginActivity

---
+++ app/src/main/java/com/example/screens/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.screens;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.screens.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
